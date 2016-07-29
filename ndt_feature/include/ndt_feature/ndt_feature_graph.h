30177d33771c2b784370fb22edc43c2609ffbe4a
Malcolm Mielle <malcolm.mielle@protonmail.com>
2016-07-30 00:45:12 +0200

    Small covariance if nothing changed

--- ndt_feature/include/ndt_feature/ndt_feature_graph.h
+++ ndt_feature/include/ndt_feature/ndt_feature_graph.h
@@ -362,6 +362,9 @@ public:
 	else{
 // 		std::cout << "NOTHING HAPPENED Creating a identity matrix" << std::endl;
 		cov = Eigen::MatrixXd::Identity(6, 6);
+		cov << 	0.02, 	0, 		0,
+				0, 		0.02, 	0,
+				0, 		0, 		0.02;
 // 		exit(0);
 	}
 // 	std::cout << "Size of Covariance : " << cov.rows() << " AND COLS " << cov.cols() << std::endl;
