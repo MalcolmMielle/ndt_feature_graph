018e80aff29e7ab2274eed2ad36b4c0864547ab1
Malcolm Mielle <malcolm.mielle@protonmail.com>
2016-09-05 23:25:07 +0200

    Skeleton of the main frame class

---
+++ ndt_feature/malcolm_addons/include/PriorAutoComplete.hpp
@@ -0,0 +1,31 @@
+#ifndef NDTFEATURE_PRIORAUTOCOMPLETE_05092016
+#define NDTFEATURE_PRIORAUTOCOMPLETE_05092016
+
+#include "das/RANSACAndCPD.hpp"
+#include "das/RANSAC.hpp"
+#include "das/priors/BasementPriorLine.hpp"
+#include "G2OGraphMaker.hpp"
+
+
+
+
+/**
+ * @brief class where the whole autocomplete of the slam is produced
+ * 
+ */
+class PriorAutoComplete{
+	
+protected:
+	
+	
+public:
+	PriorAutoComplete(){};
+	
+	
+	
+	
+	
+};
+
+
+#endif
