e433daeb84added6fb10736f82fc540f8a2efdac
Henrik Andreasson <henrik.andreasson@oru.se>
2017-03-22 13:48:20 +0100

    Added missing file·

---
+++ ndt_feature/include/ndt_feature/ndt_matcher_d2d_fusion_linesearch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <ndt_registration/ndt_matcher_d2d_2d.h>
+#include <ndt_registration/ndt_matcher_d2d_feature.h>
+#include <ndt_feature/utils.h>
+
+namespace lslgeneric {
+
+
+} //namespace
